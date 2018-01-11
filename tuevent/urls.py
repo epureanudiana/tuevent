682eb419d1d33656faa96e5c857727449440551a
vcucu <veronika.cucorova@gmail.com>
2018-01-11 15:43:07 +0100

    adjusted urls so that /app is not required to access index

--- tuevent/urls.py
+++ tuevent/urls.py
@@ -17,6 +17,6 @@ from django.urls import include, path
 from django.contrib import admin
 
 urlpatterns = [
-    path('app/', include('app.urls')),
+    path('', include('app.urls')),
     path('admin/', admin.site.urls),
 ]
