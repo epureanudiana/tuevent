5e43cff3eda4972e645cd9a681e646a2b37c2241
vcucu <veronika.cucorova@gmail.com>
2018-01-12 14:44:37 +0100

    Joined registration files to the tuevent project

--- app/urls.py
+++ app/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.conf.urls import url, include
 
 from . import views
 app_name = 'app'
@@ -9,5 +10,19 @@ urlpatterns = [
     path('professional', views.professional, name='professional'),
     path('personal', views.personal, name='personal'),
     path('myprofile', views.myprofile, name='myprofile'),
-    path('create', views.EventCreate.as_view(), name='event-add')
+    path('create', views.EventCreate.as_view(), name='event-add'),
+    path('welcome', views.welcome, name='welcome'),
+    path('register', views.register, name='register'),
+    path('register/success', views.register_success, name='register_success'),
+    #path('accounts/login', views.login, name='login'), #!!!neeeds to be defined in the views - missing so far
+    path('welcome', views.welcome, name='welcome'),
+    path('logout', views.logout_page, name='logout_page'),
+
+
+    #url(r'^register/$', register, name='register'),
+    #url(r'^register/success/$', register_success, name='register_success'),
+    #url(r'^accounts/login/$',login, name='login'),
+    #url(r'^logout/$', logout_page, name='logout_page'),
+    #url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+
 ]
--- app/views.py
+++ app/views.py
@@ -1,7 +1,19 @@
+#views.py
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import generic
 from django.views.generic.edit import CreateView, UpdateView
+
+from app.forms import *
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
+from django.views.decorators.csrf import csrf_protect
+
+from django.template import RequestContext
+from django.shortcuts import render, get_object_or_404
+
+from django.db.models import F
+from django.conf import settings
 # Create your views here.
 from .models import Event
 
@@ -28,3 +40,51 @@ def myprofile(request):
 class EventCreate(CreateView):
     model = Event
     fields = ['published_by', 'event_name', 'event_location', 'event_date', 'event_description']
+
+
+
+
+# Create your views here.
+
+
+
+
+@csrf_protect
+def register(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            user = User.objects.create_user(
+            username=form.cleaned_data['username'],
+            password=form.cleaned_data['password1'],
+            email=form.cleaned_data['email']
+            )
+            return HttpResponseRedirect('/register/success/')
+    else:
+        form = RegistrationForm()
+    variables = {
+        'form': form
+    }
+
+    return render( request,
+    'registration/register.html',
+    variables,
+    )
+
+def register_success(request):
+    return render( request,
+    'registration/success.html',
+    )
+
+def logout_page(request):
+    logout(request)
+    return HttpResponseRedirect('/')
+
+@login_required
+def welcome(request):
+    #user = request.user
+    return render(request, 'app/welcome.html')
+    #return render(request,
+    #'home.html',
+    #{ 'user': request.user }
+    #)
