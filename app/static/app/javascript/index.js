fea15db4b332321f258f49002d9fd86426999102
snazboz <omar.alabbasi97@gmail.com>
2018-01-14 00:30:05 +0100

    Diana's commits 2

--- app/static/app/javascript/index.js
+++ app/static/app/javascript/index.js
@@ -1,99 +1,3 @@
-<<<<<<< HEAD
-$(document).ready(function() {
-
-
-  $("#lei").hover(
-    // Mouse Over
-    function() {
-      $(this).animate({
-          width: 820,
-          height: 1020
-        }, 1000),
-        $("#ed").css("opacity", "0.5"),
-        $("#prf").css("opacity", "0.5"),
-        $("#per").css("opacity", "0.5");
-    },
-    // Mouse Out
-    function() {
-      $(this).animate({
-          width: 250,
-          height: 430
-        }, 1000), $("#ed").css("opacity", "1"),
-        $("#prf").css("opacity", "1"),
-        $("#per").css("opacity", "1");
-    });
-  $("#per").hover(
-    // Mouse Over
-    function() {
-      $(this).animate({
-          width: 320,
-          height: 520
-        }, 1000),
-        $("#ed").css("opacity", "0.5"),
-        $("#prf").css("opacity", "0.5"),
-        $("#lei").css("opacity", "0.5");
-    },
-    // Mouse Out
-    function() {
-      $(this).animate({
-          width: 250,
-          height: 430
-        }, 1000), $("#ed").css("opacity", "1"),
-        $("#prf").css("opacity", "1"),
-        $("#lei").css("opacity", "1");
-    });
-  $("#ed").hover(
-    // Mouse Over
-    function() {
-      $(this).animate({
-          width: 320,
-          height: 520
-        }, 1000),
-        $("#lei").css("opacity", "0.5"),
-        $("#prf").css("opacity", "0.5"),
-        $("#per").css("opacity", "0.5");
-    },
-    // Mouse Out
-    function() {
-      $(this).animate({
-          width: 250,
-          height: 430
-        }, 1000), $("#lei").css("opacity", "1"),
-        $("#prf").css("opacity", "1"),
-        $("#per").css("opacity", "1");
-    });
-  $("#prf").hover(
-    // Mouse Over
-    function() {
-      $(this).animate({
-          width: 320,
-          height: 520
-        }, 1000),
-        $("#ed").css("opacity", "0.5"),
-        $("#lei").css("opacity", "0.5"),
-        $("#per").css("opacity", "0.5");
-    },
-    // Mouse Out
-    function() {
-      $(this).animate({
-          width: 250,
-          height: 430
-        }, 1000), $("#ed").css("opacity", "1"),
-        $("#lei").css("opacity", "1"),
-        $("#per").css("opacity", "1");
-    });
-  $(".slide-section").click(function(e) {
-    var linkHref = $(this).attr("href");
-    $('html,body').animate({
-      scrollTop: $(linkHref).offset().top
-    }, 10000);
-    e.preventDefault();
-  });
-  $(document).ready(function() {
-    $('.hb-button').on('click', function() {
-      $('nav ul').toggleClass('show');
-      $('.hb-button').css('z-index', '1');
-=======
 $(document).ready(function(){
 $("#lei").hover(
   // Mouse Over
@@ -107,9 +11,9 @@ $("#lei").hover(
   // Mouse Out
   function(){
 
+      $("#ed").css("opacity","1"),
       $("#prf").css("opacity","1"),
-      $("#per").css("opacity","1"),
-      $("#ed").css("opacity","0.5");
+      $("#per").css("opacity","1");
 });
 
 $("#per").hover(
@@ -123,9 +27,8 @@ $("#per").hover(
   },
   // Mouse Out
   function(){
-
-      $("#prf").css("opacity","1"),
       $("#ed").css("opacity","1"),
+      $("#prf").css("opacity","1"),
       $("#lei").css("opacity","1");
 });
 $("#ed").hover(
@@ -174,14 +77,7 @@ $("#prf").hover(
         $('nav ul').toggleClass('show');
         $('.hb-button').css('z-index','1');
 
->>>>>>> parent of ac2ec5f... Revert "commit"
     });
-  });
+});
 
-<<<<<<< HEAD
-
-
-
-=======
->>>>>>> parent of ac2ec5f... Revert "commit"
 });
