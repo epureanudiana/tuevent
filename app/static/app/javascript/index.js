a68f3ac9ffef3c661700b40c4062beca2b6f070d
snazboz <omar.alabbasi97@gmail.com>
2018-01-13 23:34:29 +0100

    Diana's commits

--- app/static/app/javascript/index.js
+++ app/static/app/javascript/index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 $(document).ready(function() {
 
 
@@ -92,10 +93,95 @@ $(document).ready(function() {
     $('.hb-button').on('click', function() {
       $('nav ul').toggleClass('show');
       $('.hb-button').css('z-index', '1');
+=======
+$(document).ready(function(){
+$("#lei").hover(
+  // Mouse Over
+  function(){
+
+    $("#ed").css("opacity","0.5"),
+    $("#prf").css("opacity","0.5"),
+    $("#per").css("opacity","0.5");
+
+  },
+  // Mouse Out
+  function(){
+
+      $("#prf").css("opacity","1"),
+      $("#per").css("opacity","1"),
+      $("#ed").css("opacity","0.5");
+});
+
+$("#per").hover(
+  // Mouse Over
+  function(){
+
+    $("#ed").css("opacity","0.5"),
+    $("#prf").css("opacity","0.5"),
+    $("#lei").css("opacity","0.5");
+
+  },
+  // Mouse Out
+  function(){
+
+      $("#prf").css("opacity","1"),
+      $("#ed").css("opacity","1"),
+      $("#lei").css("opacity","1");
+});
+$("#ed").hover(
+  // Mouse Over
+  function(){
+
+    $("#lei").css("opacity","0.5"),
+    $("#prf").css("opacity","0.5"),
+    $("#per").css("opacity","0.5");
+
+  },
+  // Mouse Out
+  function(){
+      $("#lei").css("opacity","1"),
+      $("#prf").css("opacity","1"),
+      $("#per").css("opacity","1");
+});
+
+$("#prf").hover(
+  // Mouse Over
+  function(){
+
+    $("#ed").css("opacity","0.5"),
+    $("#lei").css("opacity","0.5"),
+    $("#per").css("opacity","0.5");
+
+  },
+  // Mouse Out
+  function(){
+      $("#ed").css("opacity","1"),
+      $("#lei").css("opacity","1"),
+      $("#per").css("opacity","1");
+});
+
+  $(".slide-section").click(function(e){
+
+      var linkHref = $(this).attr("href");
+      $('html,body').animate({
+         scrollTop: $(linkHref).offset().top
+      }, 1000);
+      e.preventDefault();
+  });
+
+    $(document).ready(function(){
+    $('.hb-button').on('click', function(){
+        $('nav ul').toggleClass('show');
+        $('.hb-button').css('z-index','1');
+
+>>>>>>> parent of ac2ec5f... Revert "commit"
     });
   });
 
+<<<<<<< HEAD
 
 
 
+=======
+>>>>>>> parent of ac2ec5f... Revert "commit"
 });
