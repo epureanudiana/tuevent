e187dbaf1c50ddcc5760efa8b469bd96198119d2
Cucorová <v.cucorova@student.tue.nl>
2018-01-14 21:37:19 +0100

    Added google maps API

--- app/models.py
+++ app/models.py
@@ -14,7 +14,7 @@ class Event(models.Model):
     published_by = models.ForeignKey(User, on_delete=models.CASCADE)
     event_name = models.CharField(max_length=200)
     event_location = models.CharField(max_length=250)
-    #position = GeopositionField()
+    position = GeopositionField(default='51.447,5.484')
     event_date = models.DateTimeField('Date_format: YYYY-MM-DD HH:mm:ss')
     event_description = models.CharField(max_length=300)
     event_category = models.CharField(
