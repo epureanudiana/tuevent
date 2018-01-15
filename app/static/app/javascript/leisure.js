2c1591f07643fc21415660a73c3adc0ed2089add
Diana <epureanudiana13@gmail.com>
2018-01-15 14:23:01 +0100

    User can login by pressing enter, without necessarily clicking on "Login" button

--- app/static/app/javascript/leisure.js
+++ app/static/app/javascript/leisure.js
@@ -1,22 +1,28 @@
 $(document).ready(function(){
     $(".slide-section").click(function(e){
-      
+
       var linkHref = $(this).attr("href");
       $('html,body').animate({
-         scrollTop: $(linkHref).offset().top 
+         scrollTop: $(linkHref).offset().top
       }, 1000);
       e.preventDefault();
   });
-    
+
+  $("#console").keyup(function(event){
+    if(event.keyCode == 13) {
+        $("#submitField").click();
+    }
+});
+
     $(document).ready(function(){
     $('.hb-button').on('click', function(){
         $('nav ul').toggleClass('show');
         $('.hb-button').css('z-index','1');
-        
+
     });
     $('.btn-group>.btn').click(function(){
-       $(this).addClass("active").siblings().removeClass("active"); 
-    });    
-        
+       $(this).addClass("active").siblings().removeClass("active");
+    });
+
 });
 });
